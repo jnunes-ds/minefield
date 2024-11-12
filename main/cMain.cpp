4df10c64a6af03c794fe4e47551fbf5da660f891
Júnior Nunes <denisonprobable13@gmail.com>
2024-11-12 15:42:25 -0300

    feat: button press working, application simulate a todo list for now

--- main/cMain.cpp
+++ main/cMain.cpp
@@ -1,12 +1,22 @@
 #include "cMain.h"
 
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+  EVT_BUTTON(10001, cMain::OnButtonClicked)
+wxEND_EVENT_TABLE()
+
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "jnunes.com - minefield")
 {
-  m_btn1 = new wxButton(this, wxID_ANY, "Click here!", wxPoint(10, 10), wxSize(150, 50));
+  m_btn1 = new wxButton(this, 10001, "Click here!", wxPoint(10, 10), wxSize(150, 50));
   m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
   m_list = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
 }
 
 cMain::~cMain()
 {
+}
+
+void cMain::OnButtonClicked(wxCommandEvent &evt)
+{
+  m_list->AppendString(m_txt1->GetValue());
+  evt.Skip();
 }
--- main/cMain.h
+++ main/cMain.h
@@ -13,6 +13,10 @@ public:
   wxButton* m_btn1 = nullptr;
   wxTextCtrl* m_txt1 = nullptr;
   wxListBox* m_list = nullptr;
+
+  void OnButtonClicked(wxCommandEvent &evt);
+
+  wxDECLARE_EVENT_TABLE();
 };
 
 
